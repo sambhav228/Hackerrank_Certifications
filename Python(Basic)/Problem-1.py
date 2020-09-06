ee9a6f7745062fb78fc9824c112583e6c2f776c6
Sambhav Thakur <sambhavkumar228@gmail.com>
2020-09-06 20:12:40 +0530

    Update Problem-1.py

--- Python(Basic)/Problem-1.py
+++ Python(Basic)/Problem-1.py
@@ -1,1 +1,13 @@
-.
+//@sambhav228
+
+//Algorithm Discussion on Problem 1
+
+/* The below problem solving algorithm approach is mine ; it's upto a programmer how efficiently they can optimize the solution. */
+
+/*
+The first coding problem in my assessment test was about :
+                                                          Finding the average value of a list elements
+    
+    I solved this problem using in-built function called as mean().*/
+    
+ As this was 1st problem so they kept straight-forward Problem Statemets which I told earlier was based on finding avg. value of a list elements.
