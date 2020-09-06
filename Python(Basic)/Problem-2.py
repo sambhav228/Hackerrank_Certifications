c9588468584373f5fe5a4576e2cc71e7f0f0ed27
Sambhav Thakur <sambhavkumar228@gmail.com>
2020-09-06 20:23:46 +0530

    Update Problem-2.py

--- Python(Basic)/Problem-2.py
+++ Python(Basic)/Problem-2.py
@@ -1,1 +1,18 @@
+//@sambhav228
 
+//Algorithm Discussion on Problem 2
+
+/* The below problem solving algorithm approach is mine ; it's upto a programmer how efficiently they can optimize the solution. */
+
+/*
+The second coding problem in my assessment test was about :
+  
+                                                            It was based on Oops concepts 
+                                                            I need to implements remove,insert, update and delete userdefine functions for the given problem statements.
+      
+                                                            I have done this problem using the class and object Oops concepts.
+                                                            As they already provided the usedefine functions name so coming to implementation part it was not too tough to frame a 
+                                                            required solution for the given problem statements.
+                                                        
+  A basic knowledge of python which gives you overview of all topics can successfully complete this certification .
+                                                                                                    Happy Coding !!!
